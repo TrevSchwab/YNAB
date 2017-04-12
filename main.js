547ebad2e791e3e740504ab3e96fc54cec64bca6
Trevor Schwab <trevschwab@gmail.com>
2017-04-12 16:25:49 -0500

    if/else background color program
    feels good

--- main.js
+++ main.js
@@ -2,7 +2,11 @@
 const budgetCheckbox = document.querySelectorAll('.budget-content input[type="checkbox"]');
 
 function selectRow() {
-  this.closest('tr').classList.add('selected');
+  if (this.checked) {
+    this.closest('tr').classList.add('selected');
+  } else {
+  this.closest('tr').classList.remove('selected');
+  }
 }
 
 budgetCheckbox.forEach((item) => {
