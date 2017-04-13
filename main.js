6ff7af7f3fe73c4daffb1fff1757d1d9b9297a98
Trevor Schwab <trevschwab@gmail.com>
2017-04-13 15:41:13 -0500

    simultaneous check on input and row!

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-
 const budgetCheckbox = document.querySelectorAll('.budget-content input[type="checkbox"]');
+const budgetRow = document.querySelectorAll('.sub-category')
 
-function selectRow() {
+function selectCheckbox() {
   if (this.checked) {
     this.closest('tr').classList.add('selected');
   } else {
@@ -9,6 +9,22 @@ function selectRow() {
   }
 }
 
+function selectRow() {
+  const checkbox = this.querySelector('input[type="checkbox"]');
+  checkbox.checked = !checkbox.checked;
+
+  if (checkbox.checked) {
+    this.classList.add('selected');
+  } else {
+    this.classList.remove('selected');
+  }
+}
+
 budgetCheckbox.forEach((item) => {
-  item.addEventListener('change', selectRow);
+  item.addEventListener('change', selectCheckbox);
+  item.addEventListener('click', (event) => event.stopPropagation());
+});
+
+budgetRow.forEach((item) => {
+  item.addEventListener('click', selectRow);
 });
